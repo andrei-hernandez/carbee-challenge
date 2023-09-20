60d32c036ce1282db96889436386e96d4780a3eb
Andrei Hernandez <andreihernandez@Andreis-MacBook-Pro.local>
2023-09-20 11:14:05 -0600

    enabled pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
 import "antd/dist/reset.css"
 import "@/styles/globals.sass"
 
-export default function App ({ Component, pageProps }: AppProps) {
+export default function App ({ Component, pageProps }: AppProps): React.JSX.Element {
   return <Component {...pageProps} />
 }
